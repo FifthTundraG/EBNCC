57ae3aac50c8fc89c55bb0399ee838c35eb00679
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2025-12-30 15:09:36 -0500

    Boilerplate

---
+++ src/main/java/me/frogtato/ebncc/EBNCC.java
@@ -0,0 +1,10 @@
+package me.frogtato.ebncc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class EBNCC implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello from EBNCC!");
+    }
+}
